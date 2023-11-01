81551d35332c1d8aa7af00a3ac728f91ea2d2082
Dhananjay <dhananjayjadhav2151@gmail.com>
2023-11-01 18:05:07 +0530

    transaction and authentication added

---
+++ src/main/java/com/app/ms/utility/AuthenticationManager.java
@@ -0,0 +1,20 @@
+package com.app.ms.utility;
+
+public class AuthenticationManager {
+
+	
+	public boolean authenticateUser() {
+		return false;
+	}
+	
+	public boolean authorizeUser() {
+		
+		return false;
+	}
+	public boolean verifyCredentials(){
+		
+		return false;
+	}
+	
+	
+}
