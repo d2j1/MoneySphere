41ee943e54f18994d5cfc3d877b01280524bcdd4
Dhananjay <dhananjayjadhav2151@gmail.com>
2023-11-05 20:08:00 +0530

    notification and security manager services added

---
+++ src/main/java/com/app/ms/utility/NotificationService.java
@@ -0,0 +1,15 @@
+package com.app.ms.utility;
+
+public class NotificationService {
+
+	
+	// to send notification to the email
+	public void sendEmailNotification() {
+		
+	}
+	
+	// to send notification to the mobile
+	public void sendSMSNotification(){
+		
+	}
+}
---
+++ src/main/java/com/app/ms/utility/SecurityManager.java
@@ -0,0 +1,23 @@
+package com.app.ms.utility;
+
+public class SecurityManager {
+
+	
+	// returns encrypted data
+	public String encryptData() {
+		
+		return null;
+	}
+	
+	// returns decrypted data
+	public String decryptData() {
+		return null;
+	}
+	
+	// generates random OTP
+	public String generateRandomOTP()
+	{
+		return null;
+	}
+	
+}
