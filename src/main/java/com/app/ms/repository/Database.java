11f52d989d33e5bc47b5b3b38b5ef0e5291fe93a
Dhananjay <dhananjayjadhav2151@gmail.com>
2023-11-06 21:48:59 +0530

    repository added

---
+++ src/main/java/com/app/ms/repository/Database.java
@@ -0,0 +1,29 @@
+package com.app.ms.repository;
+
+public class Database {
+
+	
+	
+	// used to save the data.
+	public boolean saveData() {
+		return false;
+	}
+	
+	
+	// It returns retireved data
+	public void retrieveData() {
+		
+	}
+	
+	
+	// to update specified data
+	public boolean updateData() {
+		return false;
+	}
+	
+	
+	// to delete specified data
+	public boolean deleteData(){
+	return false;	
+	}
+}
