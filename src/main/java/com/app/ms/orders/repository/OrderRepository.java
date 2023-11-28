8718b83d0754c8befa831ab3c48caaee8938c3bf
Dhananjay <dhananjayjadhav2151@gmail.com>
2023-11-28 09:21:24 +0530

    major update

--- src/main/java/com/app/ms/orders/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.ms.orders.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.ms.orders.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Integer>{
-
-}
